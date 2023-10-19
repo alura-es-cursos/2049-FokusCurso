486ec5582e53d0fd2b22d95a1e3e484b23c6b0f4
Barbara796 <barbara6.dossantos@gmail.com>
2023-10-19 10:10:48 -0300

    aula 2

--- script.js
+++ script.js
@@ -4,39 +4,22 @@ const botonCorto = document.querySelector('.app__card-button--corto');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
-const botones = document.querySelectorAll('.app__card-button');
-const inputMusicaEnfoque = document.querySelector('#alternar-musica');
-const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
 
-musica.loop = true;
-
-inputMusicaEnfoque.addEventListener('change', () => {
-    if(musica.paused) {
-        musica.play();
-    } else {
-        musica.pause();
-    }
-});
 
 botonEnfoque.addEventListener('click', () => {
     cambiarContexto('enfoque');
-    botonEnfoque.classList.add('active');
 });
 
 botonCorto.addEventListener('click', () => {
     cambiarContexto('descanso-corto');
-    botonCorto.classList.add('active');
 });
 
 botonLargo.addEventListener('click', () => {
     cambiarContexto('descanso-largo');
-    botonLargo.classList.add('active');
 });
 
 function cambiarContexto(contexto) {
-    botones.forEach(function (botonContexto){
-        botonContexto.classList.remove('active');
-    });
+    
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagenes/${contexto}.png`);
     switch (contexto) {
