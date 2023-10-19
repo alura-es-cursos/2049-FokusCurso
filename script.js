f1e014a63d7b68a5fa1e128fe9c2c3a42bebd64b
Barbara796 <barbara6.dossantos@gmail.com>
2023-10-19 09:48:45 -0300

    aula 3

--- script.js
+++ script.js
@@ -5,19 +5,7 @@ const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
 const botones = document.querySelectorAll('.app__card-button');
-const botonIniciarPausar = document.querySelector('#start-pause');
-const inputMusicaEnfoque = document.querySelector('#alternar-musica');
-const textoIniciarPausar = document.querySelector('#start-pause span');
-const iconoIniciarPausar = document.querySelector(".app__card-primary-butto-icon");
-const tiempoEnPantalla = document.querySelector('#timer');
-
 const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
-const audioPlay = new Audio('./sonidos/play.wav');
-const audioPausa = new Audio('./sonidos/pause.mp3');
-const audioTiempoFinalizado = new Audio('./sonidos/beep.mp3');
-
-let tiempoTranscurridoEnSegundos = 1500;
-let idIntervalo = null;
 
 musica.loop = true;
 
@@ -30,25 +18,21 @@ inputMusicaEnfoque.addEventListener('change', () => {
 });
 
 botonEnfoque.addEventListener('click', () => {
-    tiempoTranscurridoEnSegundos = 1500;
     cambiarContexto('enfoque');
     botonEnfoque.classList.add('active');
 });
 
 botonCorto.addEventListener('click', () => {
-    tiempoTranscurridoEnSegundos = 300;
     cambiarContexto('descanso-corto');
     botonCorto.classList.add('active');
 });
 
 botonLargo.addEventListener('click', () => {
-    tiempoTranscurridoEnSegundos = 900;
     cambiarContexto('descanso-largo');
     botonLargo.classList.add('active');
 });
 
 function cambiarContexto(contexto) {
-    mostrarTiempo();
     botones.forEach(function (botonContexto){
         botonContexto.classList.remove('active');
     });
@@ -76,42 +60,4 @@ function cambiarContexto(contexto) {
     }
 }
 
-const cuentaRegresiva = () => {
-    if(tiempoTranscurridoEnSegundos <= 0){
-        audioTiempoFinalizado.play();
-        alert('¡Tiempo finalizado!');
-        reiniciar();
-        return;
-    }
-    tiempoTranscurridoEnSegundos -= 1;
-    mostrarTiempo();
-};
 
-botonIniciarPausar.addEventListener('click', iniciarOpausar);
-
-function iniciarOpausar() {
-    if(idIntervalo){
-        audioPausa.play();
-        reiniciar();
-        return;
-    }
-    audioPlay.play();
-    idIntervalo = setInterval(cuentaRegresiva, 1000);
-    textoIniciarPausar.textContent = "Pausar";
-    iconoIniciarPausar.setAttribute('src', `/imagenes/pause.png`);
-}
-
-function reiniciar() {
-    clearInterval(idIntervalo); 
-    textoIniciarPausar.textContent = "Comenzar";
-    iconoIniciarPausar.setAttribute('src', `/imagenes/play_arrow.png`);
-    idIntervalo = null;
-}
-
-function mostrarTiempo() {
-    const tiempo = new Date(tiempoTranscurridoEnSegundos * 1000);
-    const tiempoFormateado = tiempo.toLocaleTimeString('es-ES', {minute: '2-digit', second: '2-digit'});
-    tiempoEnPantalla.innerHTML = `${tiempoFormateado}`;
-}
-
-mostrarTiempo();
